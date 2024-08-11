21308ad75b1460ecd1336877d6979c5553b10f2c
Knifin <angie.t.seattle@gmail.com>
2024-08-10 22:59:30 -0700

    Create environment variables.

--- digital-grimoire/src/main.ts
+++ digital-grimoire/src/main.ts
@@ -3,14 +3,24 @@ import GameEngine from './Models/Game/GameEngine.ts'
 
 class main {
     constructor() {
-        document.querySelector<HTMLDivElement>('#app')!.innerHTML = `Hello World!`
+        const url = import.meta.env.VITE_ROLES_API_URL;
+
+        document.querySelector<HTMLDivElement>('#app')!.innerHTML =
+            `
+            <p>
+                Hello World!
+            </p>
+            <p>
+                API URL: ${url}
+            </p>
+            `;
     }
 
-    public async startGame(): Promise<GameEngine> {
-        return GameEngine.init(window.screen,1,'./trouble_brewing.json');
+    public async startGame() {
+        const game: GameEngine = await GameEngine.init(window.screen,1,'./trouble_brewing.json');
+        console.log(game.scriptSheetRoles);
     }
 }
 
 const app: main = new main();
-const game: GameEngine = await app.startGame();
-console.log(game.scriptSheetRoles);
+app.startGame();
