13503e45d4e3503e76ac49930d1bb044b4d3cbf2
Knifin <angie.t.seattle@gmail.com>
2024-08-11 22:20:57 -0700

    Updated NightSheet to include a set of roles

--- digital-grimoire/src/Models/Physical/NightSheet.ts
+++ digital-grimoire/src/Models/Physical/NightSheet.ts
@@ -1,3 +1,13 @@
+import {Role} from "../Game/Role.ts";
+
 export default class NightSheet {
-    constructor() {}
+    private readonly _roles: Set<Role>;
+
+    constructor(roles: Set<Role>) {
+        this._roles = roles;
+    }
+
+    public get roles(): Set<Role> {
+        return this._roles;
+    }
 }
