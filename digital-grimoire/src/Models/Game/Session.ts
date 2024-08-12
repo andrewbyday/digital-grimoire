e6d8be9f27e849fcb5b0ddac4e72cb08d0b025f9
Knifin <angie.t.seattle@gmail.com>
2024-08-11 17:23:17 -0700

    Integrated WebSocket into Session

--- digital-grimoire/src/Models/Game/Session.ts
+++ digital-grimoire/src/Models/Game/Session.ts
@@ -1,15 +1,21 @@
 export default class Session {
     private readonly _id: number;
+    private readonly _webSocket: WebSocket;
 
     constructor() {
         this._id = this.generateId();
+        this._webSocket = new WebSocket("ws://localhost:8080");
     }
 
     public get id(): number {
         return this._id;
     }
 
-    public generateId() {
+    public get webSocket(): WebSocket {
+        return this._webSocket;
+    }
+
+    private generateId() {
         return 0;
     }
 }
