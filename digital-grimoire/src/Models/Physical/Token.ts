5a6846a07ed5fc1d9e552ff85200a33e9bb8eff1
Knifin <angie.t.seattle@gmail.com>
2024-08-11 22:46:08 -0700

    Added token's position to the board property.

--- digital-grimoire/src/Models/Physical/Token.ts
+++ digital-grimoire/src/Models/Physical/Token.ts
@@ -3,9 +3,11 @@ import Player from "../Game/Player.ts";
 
 export default class Token {
     private _role: Role;
+    private _boardPosition: { x: number; y: number };
 
-    constructor(role: Role) {
+    constructor(role: Role, position: { x: number; y: number }) {
         this._role = role;
+        this._boardPosition = position;
     }
 
     public get role() {
@@ -14,13 +16,20 @@ export default class Token {
     public set role(role: Role) {
         this._role = role;
     }
+
+    public get boardPosition(): { x: number; y: number } {
+        return this._boardPosition;
+    }
+    public set boardPosition(position: { x: number; y: number }) {
+        this._boardPosition = position;
+    }
 }
 
 export class PlayerToken extends Token {
     private _player: Player;
 
-    constructor(role: Role, player: Player) {
-        super(role);
+    constructor(role: Role, player: Player, position: { x: number; y: number }) {
+        super(role, position);
 
         this._player = player;
     }
@@ -31,7 +40,7 @@ export class PlayerToken extends Token {
 }
 
 export class FabledToken extends Token {
-    constructor(role: Role) {
-        super(role);
+    constructor(role: Role, position: { x: number; y: number }) {
+        super(role, position);
     }
 }
