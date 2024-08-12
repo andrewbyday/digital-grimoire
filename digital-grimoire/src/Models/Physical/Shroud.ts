18debcc75e6c4f611cda4cd912d6657375fa453b
Knifin <angie.t.seattle@gmail.com>
2024-08-12 12:25:22 -0700

    Added getter/setters for the Shroud.

--- digital-grimoire/src/Models/Physical/Shroud.ts
+++ digital-grimoire/src/Models/Physical/Shroud.ts
@@ -1,4 +1,56 @@
 export default class Shroud {
-    constructor() {
+    private _height: number;
+    private _width: number;
+    private _position: { x: number, y: number };
+
+    constructor(height: number, width: number, position: { x: number; y: number }) {
+        this._height = height;
+        this._width = width;
+        this._position = position;
+    }
+
+    /**
+     * Get height of the shroud in pixels
+     */
+    public get height(): number {
+        return this._height;
+    }
+
+    /**
+     * Set height of the shroud
+     * @param {number} value pixels
+     */
+    public set height(value: number) {
+        this._height = value;
+    }
+
+    /**
+     * Get the width of the shroud in pixels
+     */
+    public get width(): number {
+        return this._width;
+    }
+
+    /**
+     * Set the width of the shroud
+     * @param {number} value pixels
+     */
+    public set width(value: number) {
+        this._width = value;
+    }
+
+    /**
+     * Get the position of the shroud
+     */
+    public get position(): { x: number, y: number } {
+        return this._position;
+    }
+
+    /**
+     * Set the position of the shroud
+     * @param {x: number, y: number} value x/y pixel coordinate on the Board
+     */
+    public set position(value: { x: number, y: number }) {
+        this._position = value;
     }
 }
