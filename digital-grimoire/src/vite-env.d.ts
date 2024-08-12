4289c5591ccf70fb4a1b3d126a90fb7fb37901bb
Knifin <angie.t.seattle@gmail.com>
2024-08-11 22:59:09 -0700

    Added in semi-colons

--- digital-grimoire/src/vite-env.d.ts
+++ digital-grimoire/src/vite-env.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_ROLES_API_URL: string
-    readonly VITE_WEBSOCKET_SERVER: string
+    readonly VITE_ROLES_API_URL: string;
+    readonly VITE_WEBSOCKET_SERVER: string;
 }
 
 interface ImportMeta {
-    readonly env: ImportMetaEnv
+    readonly env: ImportMetaEnv;
 }
