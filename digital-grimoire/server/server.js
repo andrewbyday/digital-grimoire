ac4a1f9d4ef3d0ef1d7ed637b2d924217913f21c
Knifin <angie.t.seattle@gmail.com>
2024-08-12 21:09:31 -0700

    Include socket.io. Create server and client.

---
+++ digital-grimoire/server/server.js
@@ -0,0 +1,14 @@
+import { Server } from "socket.io";
+
+const io = new Server(3000, {
+   cors: {
+      origin: 'http://localhost:5173',
+      methods: ["GET", "POST"]
+   }
+});
+
+io.on("connection", (socket) => {
+   console.log('Connected:', socket.id);
+
+   socket.emit("hello", "world");
+});
