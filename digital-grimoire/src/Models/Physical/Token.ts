799e0b129f64797d8cd9ab4bbf974cfb74f6c1b4
Knifin <angie.t.seattle@gmail.com>
2024-08-13 18:23:00 -0700

    Refactored token renderer.

--- digital-grimoire/src/Models/Physical/Token.ts
+++ digital-grimoire/src/Models/Physical/Token.ts
@@ -1,13 +1,16 @@
 import {Role} from "../Game/Role.ts";
 import Player from "../Game/Player.ts";
+import Konva from "konva";
 
 export default class Token {
     private _role: Role;
     private _boardPosition: { x: number; y: number };
+    private readonly _group: Konva.Group;
 
     constructor(role: Role, position: { x: number; y: number }) {
         this._role = role;
         this._boardPosition = position;
+        this._group = this.createGroup(role,position);
     }
 
     public get role() {
@@ -22,6 +25,63 @@ export default class Token {
     }
     public set boardPosition(position: { x: number; y: number }) {
         this._boardPosition = position;
+    }
+
+    public get group(): Konva.Group {
+        return this._group;
+    }
+
+    private createGroup(role: Role, position: { x: number; y: number }): Konva.Group {
+        const group: Konva.Group = new Konva.Group({
+            x: position.x,
+            y: position.y,
+            id: role.script_id,
+            draggable: true
+        });
+
+        let bgImage: HTMLImageElement = new Image();
+        bgImage.src = role.official_icon_bg;
+        let roleImage: HTMLImageElement = new Image();
+        roleImage.src = role.official_icon;
+
+        Promise.all([this.loadImage(bgImage), this.loadImage(roleImage)]).then((values) => {
+            const circle: Konva.Circle = new Konva.Circle({
+                x: 125/2,
+                y: 125/2,
+                radius: 125/2,
+                stroke: 'black',
+                strokeWidth: 4,
+                fill: 'white'
+            });
+            group.add(circle);
+
+            const background: Konva.Image = new Konva.Image({
+               x: 0,
+               y: 0,
+               image: bgImage,
+               width: 125,
+               height: 125
+            });
+            group.add(background);
+
+            const role: Konva.Image = new Konva.Image({
+                x: 0,
+                y: 0,
+                image: roleImage,
+                width: 125,
+                height: 125
+            });
+            group.add(role);
+        });
+
+        return group;
+    }
+
+    private loadImage(image: HTMLImageElement): Promise<HTMLImageElement> {
+        return new Promise((resolve, reject) => {
+           image.onload = image => { resolve(image); }
+           image.onerror = error => { resolve(error); }
+        });
     }
 }
 
--- digital-grimoire/src/Views/GameView.ts
+++ digital-grimoire/src/Views/GameView.ts
@@ -1,11 +1,7 @@
-import Player from "../Models/Game/Player.ts";
 import Token from "../Models/Physical/Token.ts";
-import Shroud from "../Models/Physical/Shroud.ts";
-import NightSheet from "../Models/Physical/NightSheet.ts";
 import Board from "../Models/Physical/Board.ts";
 import Konva from "konva";
 import {Stage} from "konva/lib/Stage";
-import {Layer} from "konva/lib/Layer";
 
 export default class GameView {
     private readonly _stage: Stage;
@@ -28,109 +24,11 @@ export default class GameView {
         return this._stage;
     }
 
-    public renderBoard() {
-
-    }
-
-    public renderToken(token: Token): void {
-
-    }
-
     public renderTokens(tokens: Set<Token>): void {
-        const layer: Layer = new Konva.Layer();
-        const radius: number = 125/2;
-
-        let images = new Array();
-
-        let index: number = 0;
-        tokens.forEach(token => {
-            if ( index === 0 ) {
-                images.push(this.loadImage(token.role.official_icon_bg));
-                index++;
-            }
-            images.push(this.loadImage(token.role.official_icon));
+        let layer: Konva.Layer = new Konva.Layer();
+        tokens.forEach((token: Token) => {
+            layer.add(token.group);
         });
-
-        Promise.all(images).then( (result) => {
-            let index: number = 1;
-            tokens.forEach(async (token) => {
-                const group = new Konva.Group({
-                    x: token.boardPosition.x,
-                    y: token.boardPosition.y,
-                    draggable: true
-                });
-
-                // create border
-                const circle: Konva.Circle = new Konva.Circle({
-                    x: radius,
-                    y: radius,
-                    radius: radius,
-                    stroke: 'black',
-                    strokeWidth: 4,
-                    fill: 'white',
-                });
-
-                // create image
-                const roleBg = new Konva.Image({
-                    x: 0,
-                    y: 0,
-                    width: 125,
-                    height: 125,
-                    image: result[0]
-                });
-
-                // create image
-                const roleImg = new Konva.Image({
-                    x: 0,
-                    y: 0,
-                    width: 125,
-                    height: 125,
-                    image: result[index++]
-                });
-
-                group.add(circle);
-                group.add(roleBg);
-                group.add(roleImg);
-                layer.add(group);
-            });
-            this._stage.add(layer);
-        });
-    }
-
-    public renderPlayerToken(player: Player): void {
-
-    }
-
-    public renderPlayerTokens(players: Set<Player>): void {
-
-    }
-
-    public renderShroudToken(shroud: Shroud): void {
-
-    }
-
-    public renderShroudTokens(shrouds: Set<Shroud>): void {
-
-    }
-
-    public renderNightSheet(nightSheet: NightSheet): void {
-
-    }
-
-    public renderNightSheets(nightSheets: Set<NightSheet>): void {
-
-    }
-
-    private loadImage(url: string): Promise<HTMLImageElement> {
-        return new Promise((resolve, reject) => {
-           const img: HTMLImageElement = new Image();
-           img.src = url;
-           img.onload = function() {
-               resolve(img);
-           }
-           img.onerror = function() {
-               reject(img);
-           }
-        });
+        this._stage.add(layer);
     }
 }
