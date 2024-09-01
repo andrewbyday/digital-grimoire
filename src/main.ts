d13060c6fda0d5b26e2792384a4ffb29e3a619be
Knifin <angie.t.seattle@gmail.com>
2024-09-01 15:37:29 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { io } from "socket.io-client";
 
 class main {
     constructor() {
-        const client = io("http://joinapp.shinpostudios.com:3000");
+        const client = io("https://joinapp.shinpostudios.com:3000");
         client.on('hello', (args) => {
             console.log(args);
         });
