019417e008e749181246ce9d6870f2078ad4fc1b
Knifin <angie.t.seattle@gmail.com>
2024-09-01 21:03:27 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import Session from "./Models/Game/Session.ts";
 import GameView from "./Views/GameView.ts";
 import GameController from "./Controllers/GameController.ts";
 import { io } from "socket.io-client";
+import { readFileSync } from "fs";
 
 class main {
     constructor() {
-        const client = io("https://joinapp.shinpostudios.com");
+        const client = io("https://joinapp.shinpostudios.com", {
+            ca: readFileSync('/app/server/tls/server.letsencrypt.crt')
+        });
         client.on('hello', (args) => {
             console.log(args);
         });
