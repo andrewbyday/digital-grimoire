73444cf17d4771c06936c2cb11349b6432a51669
Knifin <angie.t.seattle@gmail.com>
2024-09-17 21:30:57 -0700

    Added rendering tokens, buttons and other components.

--- src/Models/Helper/Positions.ts
+++
@@ -1,9 +0,0 @@
-export default class Positions {
-    protected _tokenSize: number;
-    protected _x: number;
-    protected _y: number;
-
-    constructor(tokenSize: number) {
-        this._tokenSize = tokenSize;
-    }
-}
