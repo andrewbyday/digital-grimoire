cce25bb736d7768240ad668cbf3da32e50b4b1b0
Knifin <angie.t.seattle@gmail.com>
2024-10-03 13:58:17 -0700

    Patched two vulnerabilities (1 moderate, 1 high)

--- src/Models/Physical/NightSheet.ts
+++ src/Models/Physical/NightSheet.ts
@@ -1,13 +1,30 @@
 import {Role} from "../Game/Role.ts";
+import Konva from "konva";
 
 export default class NightSheet {
     private readonly _roles: Set<Role>;
+    private readonly _group: Konva.Group;
 
-    constructor(roles: Set<Role>) {
+    constructor(roles: Set<Role>, type: string) {
         this._roles = roles;
+
+        let bg_url = '';
+        if (type === 'first') {
+
+        } else {
+
+        }
+
+        this._group = new Konva.Group({
+
+        });
     }
 
     public get roles(): Set<Role> {
         return this._roles;
+    }
+
+    public renderSheet(): Konva.Group {
+        return this._group;
     }
 }
