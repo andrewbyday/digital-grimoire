2e3607d17f0815e9fb1138d487f175712f65e1d1
Knifin <angie.t.seattle@gmail.com>
2024-10-06 14:03:37 -0700

    Fixed font issue.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import './style.css';
+import './assets/fonts/Dumbledore/dum1.ttf';
+
 import GameEngine from './Models/Game/GameEngine.ts'
 import Session from "./Models/Game/Session.ts";
 import GameView from "./Views/GameView.ts";
