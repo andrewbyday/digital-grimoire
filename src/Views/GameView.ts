2002335ec99a7d2cef8df8c41d05f233dc169fa2
Knifin <angie.t.seattle@gmail.com>
2024-10-10 20:49:53 -0700

    Fix bug with shrouds.

--- src/Views/GameView.ts
+++ src/Views/GameView.ts
@@ -339,11 +339,6 @@ export default class GameView {
                         shroud.group.x(10);
                         shroud.group.y(this._stage.height() - 100);
                         shroud.connected = false;
-                    } else {
-                        shroud.group.moveTo(this._shroudLayer);
-                        shroud.group.x(10);
-                        shroud.group.y(this._stage.height() - 100);
-                        shroud.group.draggable(true);
                     }
                 });
 
