5a12f0d9c9dc26e11cd4ad04b91bb2ce73bfad69
Knifin <angie.t.seattle@gmail.com>
2025-04-25 11:39:40 -0700

    Specify name and pronoun.

--- src/Views/GameView.ts
+++ src/Views/GameView.ts
@@ -301,7 +301,7 @@ export default class GameView {
             token.disableDragging();
 
             token.group.on('click tap', (): void => {
-                const newPlayer: Player = new Player('NA', 'NA', true, `${uuidv4()}`);
+                const newPlayer: Player = new Player('Name', 'Pronouns', true, `${uuidv4()}`);
 
                 const newToken: TokenPlayer = new TokenPlayer(token.role, newPlayer, {x: 50, y: bottom});
                 newToken.group.on('dragend', (): void => {
